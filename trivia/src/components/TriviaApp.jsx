7fa87fa56e8a968547c985a88022105b9bd71898
Sara Inoue <sarainoue11@gmail.com>
2024-05-15 23:13:45 -0800

    complete and updated trivia

--- trivia/src/components/TriviaApp.jsx
+++ trivia/src/components/TriviaApp.jsx
@@ -1,19 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const TriviaApp = () => {
-    const [questions, setQuestions] = useState([])
-useEffect(()=> {
-    const fetchData = async() => {
-        const result = await fetch("https://the-trivia-api.com/v2/questions");
-        const resultJson = await result.json()
-        setQuestions(resultJson.results)
-        }
-    fetchData();
-},[])
+    const [questions, setQuestions] = useState([]);
+    const [selectedAnswers, setSelectedAnswers] = useState({});
 
-  return (
-    <h1>Trvia App</h1>
-  )
-}
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch("https://the-trivia-api.com/v2/questions");
+            const resultJson = await result.json();
+            setQuestions(resultJson); // The API response is an array of questions
+        };
+        fetchData();
+    }, []);
 
-export default TriviaApp
+    const handleAnswerClick = (questionId, answer) => {
+        setSelectedAnswers((prevSelectedAnswers) => ({
+            ...prevSelectedAnswers,
+            [questionId]: answer,
+        }));
+    };
+
+    return (
+        <>
+            <h1>Trivia App</h1>
+            {questions.length > 0 ? (
+                questions.map((question) => (
+                    <div key={question.id}>
+                        <p>{question.question.text}</p>
+                        <div>
+                            {[question.correctAnswer, ...question.incorrectAnswers].sort().map((answer) => (
+                                <button
+                                    key={answer}
+                                    onClick={() => handleAnswerClick(question.id, answer)}
+                                    style={{
+                                        backgroundColor: selectedAnswers[question.id] === answer 
+                                            ? answer === question.correctAnswer 
+                                                ? 'green' 
+                                                : 'red' 
+                                            : 'initial'
+                                    }}
+                                >
+                                    {answer}
+                                </button>
+                            ))}
+                        </div>
+                        {selectedAnswers[question.id] && (
+                            <p>
+                                {selectedAnswers[question.id] === question.correctAnswer
+                                    ? 'Correct!'
+                                    : `Incorrect.`}
+                            </p>
+                        )}
+                    </div>
+                ))
+            ) : (
+                <p>Loading questions...</p>
+            )}
+        </>
+    );
+};
+
+export default TriviaApp;
